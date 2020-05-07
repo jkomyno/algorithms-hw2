6cb82e7a677cee50e0f4737b214f75f714b1a048
lparolari <luca.parolari23@gmail.com>
2020-05-07 16:06:31 +0200

    Added held & karp algorithm

--- HeldKarp/main.cpp
+++ HeldKarp/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>  // std::cout, std::endl
 
 #include "DistanceMatrix.h"
+#include "held_karp.h"
 #include "read_file.h"
 
 int main(int argc, char** argv) {
@@ -9,11 +10,15 @@ int main(int argc, char** argv) {
         exit(0);
     }
 
+    // read the graph and create the distance matrix
     const char* filename = argv[1];
     auto point_reader(read_file(filename));
     const auto distance_matrix = point_reader->create_distance_matrix();
 
-    std::cout << distance_matrix << std::endl;
+    // calculate the weight held & kart
+    const auto total_weight =
+        held_karp_tsp(std::forward<decltype(distance_matrix)>(distance_matrix));
 
-    std::cin.get();
+    // use std::fixed to avoid displaying numbers in scientific notation
+    std::cout << std::fixed << total_weight << std::endl;
 }
