2795c68f88704afcbc128b2c0e17633515c52cff
Alberto Schiabel <skiabo97@gmail.com>
2020-05-08 20:02:53 +0200

    Fixed logic error in MST2Approximation

--- MST2Approximation/approx_tsp.h
+++ MST2Approximation/approx_tsp.h
@@ -5,29 +5,21 @@
 #include "DFS.h"
 #include "DistanceMatrix.h"
 #include "prim_binary_heap_mst.h"
+#include "shared_utils.h"
 #include "sum_weights.h"
 
 [[nodiscard]] inline int approx_tsp(DistanceMatrix<int>&& distance_matrix) noexcept {
     // compute the exact Minimum Spanning Tree of the input graph using Prim's algorithm
     auto mst(mst::prim_binary_heap_mst(distance_matrix));
 
-    // we compute the weight of the mst
-    const int mst_weight = sum_weights(mst.cbegin(), mst.cend());
-
     // use DFS to perform a preorder traversal of the MST
     DFS dfs(std::move(mst));
-    const auto preorder_lst = dfs.preorder_traversal();
+    const auto circuit = dfs.preorder_traversal_rec();
 
-    // save the first node of the preorder traversal
-    const size_t first_preorder = preorder_lst.front();
+    const auto get_distance = [&distance_matrix](const size_t x, const size_t y) {
+        return distance_matrix.at(x, y);
+    };
 
-    // save last node of the preorder traversal, which is a leaf
-    const size_t last_preorder = preorder_lst.back();
-
-    // we use the original distance matrix to compute the distance between the first and the last
-    // elements of the preorder traversal
-    const int weight_first_last = distance_matrix.at(first_preorder, last_preorder);
-
-    // return the 2-approximated TSP weight
-    return mst_weight + weight_first_last;
+    // return the weights in the circuit
+    return utils::sum_weights_in_circuit(circuit.cbegin(), circuit.cend(), get_distance);
 }
