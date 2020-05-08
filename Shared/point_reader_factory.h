95664d48e7f6f94ef3f23406c39f4688b8d261c9
Alberto Schiabel <skiabo97@gmail.com>
2020-05-08 11:28:18 +0200

    Explicitly silenced false-positive warning C4715 in Shared/point_reader_factory.h

--- Shared/point_reader_factory.h
+++ Shared/point_reader_factory.h
@@ -1,5 +1,10 @@
 #pragma once
 
+// suppress warning C4715: not all control paths return a value
+#if defined(_MSC_VER)
+    #pragma warning(disable : 4715)
+#endif
+
 #include <fstream>
 #include "EdgeWeightType.h"
 
@@ -9,7 +14,7 @@
 
 namespace point_reader {
     std::unique_ptr<PointReader> point_reader_factory(EdgeWeightType edge_weight_type,
-                                                      std::fstream& file, size_t dimension) {
+                                                      std::fstream& file, size_t dimension) noexcept {
         switch (edge_weight_type) {
         case EdgeWeightType::EUC_2D:
             return std::make_unique<EuclideanPointReader>(file, dimension);
