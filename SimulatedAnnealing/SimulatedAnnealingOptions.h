8aab0c67988131e9406c31fa43de3b105e4515fb
Alberto Schiabel <skiabo97@gmail.com>
2020-05-19 23:32:36 +0200

    Added comments and new options to SimulatedAnnealingOptions.h

--- SimulatedAnnealing/SimulatedAnnealingOptions.h
+++ SimulatedAnnealing/SimulatedAnnealingOptions.h
@@ -2,30 +2,42 @@
 
 namespace simulated_annealing {
     struct SimulatedAnnealingOptions {
-        // number of iterations
-        const size_t iterations;
+        // number of annealing iterations
+        const size_t annealing_steps;
+
+        // number of iterations in an annealing step where the temperature doesn't change
+        const size_t steady_steps;
 
         // initial temperature
         const double init_temperature;
 
-        // final temperature
+        // temperature that, when reached, stops the optimization process
         const double stop_temperature;
 
-        // annealing coefficient
+        // annealing multiplicative coefficient
         const double lambda;
 
-        // periodic reheating interval
-        const size_t reheat;
+        // the temperature will be re-heated every reheat_interval annealing steps
+        const size_t reheat_interval;
 
-        explicit SimulatedAnnealingOptions(size_t iterations = 100000,
-                                           double init_temperature = 1000,
-                                           double stop_temperature = 1e-16, double lambda = 0.995,
-                                           size_t reheat = 10000) :
-            iterations(iterations),
+        // factor by which the temperature will be multiplied during reheating
+        const double reheat_factor;
+
+        /**
+         * These options seem to work best when the graphs have less than 300 nodes.
+         */
+        explicit SimulatedAnnealingOptions(size_t annealing_steps = 100000, size_t steady_steps = 5,
+                                           double init_temperature = 10000,
+                                           double stop_temperature = 1e-16, double lambda = 0.992,
+                                           size_t reheat_interval = 2500,
+                                           double reheat_factor = 10e3) :
+            annealing_steps(annealing_steps),
+            steady_steps(steady_steps),
             init_temperature(init_temperature),
             stop_temperature(stop_temperature),
             lambda(lambda),
-            reheat(reheat) {
+            reheat_interval(reheat_interval),
+            reheat_factor(reheat_factor) {
         }
     };
 }  // namespace simulated_annealing
