4f328d8f4fdfbbe1d9835e409bd241b4088589e7
Alberto Schiabel <skiabo97@gmail.com>
2020-05-31 14:56:24 +0200

    Introduced random_generator hierarchy

--- Shared/random_generator.h
+++ Shared/random_generator.h
@@ -3,38 +3,70 @@
 #include <random>
 
 namespace random_generator {
-    class RealRandomGenerator {
+    /**
+     * Generic random generator abstract class.
+     */
+    template <typename T>
+    class RandomGenerator {
+    protected:
         std::mt19937 engine{std::random_device()()};
+
+    public:
+        // virtual destructor because this is a base class
+        virtual ~RandomGenerator() noexcept = default;
+
+        // set the seed of the random generator
+        void set_seed(unsigned int seed) {
+            engine.seed(seed);
+        }
+
+        // return a random value of generic type T
+        virtual T operator()() = 0;
+    };
+
+    /**
+     * Real number random generator that returns values in range [first, last]
+     */
+    class RealRandomGenerator : public RandomGenerator<double> {
         std::uniform_real_distribution<double> dist;
 
     public:
-        RealRandomGenerator(double x, double y) : dist(x, y) {
+        RealRandomGenerator(double first, double last) noexcept : dist(first, last) {
         }
 
-        void set_seed(unsigned int seed) {
-            engine.seed(seed);
-        }
-
-        double operator()() {
-            return dist(engine);
+        double operator()() override {
+            return dist(this->engine);
         }
     };
 
-    class IntegerRandomGenerator {
-        std::mt19937 engine{std::random_device()()};
+    /**
+     * Unsigned integer random generator that returns values in range [first, last]
+     */
+    class IntegerRandomGenerator : public RandomGenerator<size_t> {
         std::uniform_int_distribution<size_t> dist;
 
     public:
-        // creates a random generator that can return random integers in the range [x, y]
-        IntegerRandomGenerator(size_t x, size_t y) : dist(x, y) {
+        IntegerRandomGenerator(size_t first, size_t last) noexcept : dist(first, last) {
         }
 
-        void set_seed(unsigned int seed) {
-            engine.seed(seed);
+        size_t operator()() override {
+            return dist(this->engine);
+        }
+    };
+
+    /**
+     * Mock generator that implements RandomGenerator but returns a fixed value
+     */
+    template <typename T>
+    class FixedGenerator : public RandomGenerator<T> {
+        T fixed_value;
+
+    public:
+        explicit FixedGenerator(T fixed_value) noexcept : fixed_value(fixed_value) {
         }
 
-        size_t operator()() {
-            return dist(engine);
-        }
+        T operator()() override {
+            return fixed_value;
+        };
     };
 }  // namespace random_generator
